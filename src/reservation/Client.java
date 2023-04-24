0a6ef711af61f900a100c7dbb2245020e2dab249
(en) <kezhkia@protonmail.com>
2023-04-24 10:48:53 +0200

    added code in Client and Reservation

--- src/reservation/Client.java
+++ src/reservation/Client.java
@@ -1,21 +1,76 @@
 package reservation;
 
+import java.util.UUID;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
 public class Client {
     private String nom;
+    private String ref;
 
-    public void reference(){
+    private String creditCard;
+    private String mailAddress;
 
+    private static final Pattern VALIDATION_VISA = Pattern.compile("/^4[0-9]{12}(?:[0-9]{3})?$/");
+    private static final Pattern VALIDATION_MAIL = Pattern.compile("^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$");
+
+    /**
+     *
+     */
+    public Client(){
+        this.ref = UUID.randomUUID().toString();
     }
-    public void paiement(){
 
+    /**
+     *
+     * @return
+     */
+    public String getReference(){
+        return this.ref;
     }
-    public void contact(){
 
+    /**
+     *
+     * @param cc
+     * @throws IllegalArgumentException
+     */
+    public void setPaiement(String cc) throws IllegalArgumentException{
+        Matcher m = VALIDATION_VISA.matcher(cc);
+        if (!m.matches()){
+            throw new IllegalArgumentException("Credit Card not valid.");
+        } else {
+            this.creditCard = cc;
+        }
     }
 
-    public void getNom(){
-
+    /**
+     *
+     * @return
+     */
+    public String getPaiement(){
+        return this.creditCard;
     }
+    public void setContact(String mail) throws IllegalArgumentException{
+        Matcher m = VALIDATION_VISA.matcher(mail);
+        if (!m.matches()){
+            throw new IllegalArgumentException("Mail address not valid.");
+        } else {
+            this.mailAddress = mail;
+        }
+    }
+
+    /**
+     *
+     * @return
+     */
+    public String getNom(){
+        return this.nom;
+    }
+
+    /**
+     *
+     * @param n
+     */
     public void setNom(String n){
         this.nom = n;
     }
--- src/reservation/Reservation.java
+++ src/reservation/Reservation.java
@@ -1,26 +1,45 @@
 package reservation;
 
-import java.util.Date;
+import java.util.UUID;
+import java.time.ZonedDateTime ;
 
 public class Reservation {
 
     private String client;
-    private int numero;
-    private Date date;
+    private String uniqueID;
+    private ZonedDateTime date;
+    private boolean confirmed = false;
 
-    public Reservation() {
+    public Reservation(String clientRef) {
+        this.uniqueID = UUID.randomUUID().toString();
+        this.date = ZonedDateTime.now();
+        this.client = clientRef;
     }
 
-    public void confirmer(){
-
+    public void confirmer(Client c) throws IllegalArgumentException {
+        if (!(c.getReference() == this.client)){
+            throw new IllegalArgumentException("Wrong client.");
+        }
+        if (c.getPaiement() != null || c.getNom() != null || c.getReference() != null){
+            this.confirmed = true;
+        }
     }
     public void annuler(){
-
+        this.confirmed = false;
     }
+
+    /**
+     *
+     * @return
+     */
     public String getClient() {
         return client;
     }
 
+    /**
+     *
+     * @param client
+     */
     public void setClient(String client) {
         this.client = client;
     }
