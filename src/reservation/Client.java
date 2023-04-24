f4e1b02fad282d6601d8a70e48c40e1a42cc0b53
(en) <kezhkia@protonmail.com>
2023-04-24 08:42:03 +0200

    a;lifj

---
+++ src/reservation/Client.java
@@ -0,0 +1,22 @@
+package reservation;
+
+public class Client {
+    private String nom;
+
+    public void reference(){
+
+    }
+    public void paiement(){
+
+    }
+    public void contact(){
+
+    }
+
+    public void getNom(){
+
+    }
+    public void setNom(String n){
+        this.nom = n;
+    }
+}
--- src/reservation/Reservation.java
+++ src/reservation/Reservation.java
@@ -1,12 +1,22 @@
 package reservation;
 
+import java.util.Date;
+
 public class Reservation {
 
     private String client;
+    private int numero;
+    private Date date;
 
     public Reservation() {
     }
 
+    public void confirmer(){
+
+    }
+    public void annuler(){
+
+    }
     public String getClient() {
         return client;
     }
