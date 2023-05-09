1e2824e28452c66a89d0e7996fc74f0e14837417
KPq66dw8L <kezhkia@protonmail.com>
2023-05-09 11:46:26 +0200

    Moved content from Start to MyTests. TODO in MyTests: everything in assertion, not just println + more tests

--- src/MyTests.java
+++ src/MyTests.java
@@ -1,5 +1,6 @@
 
 
+import gestionVol.Compagnie;
 import gestionVol.Vol;
 import org.testng.annotations.Test;
 import reservation.Client;
@@ -19,31 +20,56 @@ public class MyTests {
      * ./gradlew.bat test --info --rerun-tasks
      */
 
+    SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy hh:mm");
+
     @Test
     public void testClient() throws Exception {
 
-        SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy hh:mm");
+
         String dd = "21/10/2020 13:00";
         String da = "23/10/2020 02:15";
+
         Client c1 = new Client("Pablo Escobar");
         c1.setContact("pablo@gmail.com");
-        // Testing purpose: https://fossbytes.com/tools/credit-card-generator
+        // Testing purpose only: https://fossbytes.com/tools/credit-card-generator
+        // exemple VISA:
         c1.setPaiement("4272505668446363");
 
         assertThat(c1.getPaiement(), is("4272505668446363"));
         assertThat(c1.getContact(), is("pablo@gmail.com"));
+
+        Vol v1 = new Vol("AF123", format.parse(dd), format.parse(da));
+        Reservation r1 = new Reservation(c1, v1);
+        if (r1.getClient() == c1.getReference()) {
+            System.out.println(c1.getNom());
+        }
+
+        //exemple MASTERCARD:
+        Client c2 = new Client("Julien Herbaux");
+        c2.setContact("julien@gmail.com");
+        c2.setPaiement("2483571672956135");
+        Reservation r2 = new Reservation(c2, v1);
+
+        //exemple AMEX:
+        Client c3 = new Client("Axel Exposito");
+        c3.setContact("axel@gmail.com");
+        c3.setPaiement("377732303291515");
+        Reservation r3 = new Reservation(c3, v1);
     }
 
     @Test
     public void testReservation() throws Exception {
-        SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy hh:mm");
+
         String dd = "21/10/2020 13:00";
         String da = "23/10/2020 02:15";
+
         Client c1 = new Client("Pablo Escobar");
         c1.setContact("pablo@gmail.com");
         // Testing purpose: https://fossbytes.com/tools/credit-card-generator
         c1.setPaiement("4272505668446363");
+
         Vol v1 = new Vol("AF123", format.parse(dd), format.parse(da));
+
         Reservation r1 = new Reservation(c1, v1);
         if (r1.getClient() == c1.getReference()) {
             System.out.println(c1.getNom());
@@ -53,6 +79,53 @@ public class MyTests {
     @Test
     public void testPassager(){
 
+    }
+
+    @Test
+    public void testVol(){
+        Vol volFinal = new Vol();
+
+        String dd = "21/10/2020 13:00";
+        String da = "23/10/2020 02:15";
+
+        try {
+            volFinal.setDateDepart(format.parse(dd));
+            volFinal.setDateArrivee(format.parse(da));
+        } catch (Exception e){
+            throw new RuntimeException("Unable to format to date");
+        }
+
+        System.out.println(volFinal.getDateArrivee());
+        System.out.println(volFinal.getDuree().toString().substring(2));
+    }
+
+    @Test
+    public void testCompagnie(){
+        //Bidirectional
+        Vol vol = new Vol();
+        vol.setNumero("abc1");
+
+        Vol vol2 = new Vol();
+        vol2.setNumero("abc2");
+
+        Compagnie compagnie = new Compagnie();
+        compagnie.setName("Air France");
+        compagnie.addVol(vol);
+        compagnie.addVol(vol2);
+
+        for(Vol v : compagnie.getVols()){
+            System.out.println(v.getNumero());
+        }
+
+        System.out.println(vol.getCompagnie().getName());
+        System.out.println(vol2.getCompagnie().getName());
+
+        vol2.setCompagnie(null);
+        System.out.println(vol2.getCompagnie());
+
+        for(Vol v : compagnie.getVols()){
+            System.out.println(v.getNumero());
+        }
     }
 
     //Help you to handle exception. :-)
--- src/Start.java
+++ src/Start.java
@@ -11,70 +11,7 @@ import java.util.List;
 public class Start {
 
     public static void main(String[] args) throws Exception {
-        Vol volFinal = new Vol();
 
-        SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy hh:mm");
-        String dd = "21/10/2020 13:00";
-        String da = "23/10/2020 02:15";
-
-        try {
-            volFinal.setDateDepart(format.parse(dd));
-            volFinal.setDateArrivee(format.parse(da));
-        } catch (Exception e){
-            throw new RuntimeException("Unable to format to date");
-        }
-
-        System.out.println(volFinal.getDateArrivee());
-        System.out.println(volFinal.getDuree().toString().substring(2));
-
-        //Bidirectional
-        Vol vol = new Vol();
-        vol.setNumero("abc1");
-
-        Vol vol2 = new Vol();
-        vol2.setNumero("abc2");
-
-        Compagnie compagnie = new Compagnie();
-        compagnie.setName("Air France");
-        compagnie.addVol(vol);
-        compagnie.addVol(vol2);
-
-        for(Vol v : compagnie.getVols()){
-            System.out.println(v.getNumero());
-        }
-
-        System.out.println(vol.getCompagnie().getName());
-        System.out.println(vol2.getCompagnie().getName());
-
-        vol2.setCompagnie(null);
-        System.out.println(vol2.getCompagnie());
-
-        for(Vol v : compagnie.getVols()){
-            System.out.println(v.getNumero());
-        }
-
-        // exemple VISA:
-        Client c1 = new Client("Pablo Escobar");
-        c1.setContact("pablo@gmail.com");
-        // Testing purpose: https://fossbytes.com/tools/credit-card-generator
-        c1.setPaiement("4272505668446363");
-        Vol v1 = new Vol("AF123", format.parse(dd), format.parse(da));
-        Reservation r1 = new Reservation(c1, v1);
-        if (r1.getClient() == c1.getReference()) {
-            System.out.println(c1.getNom());
-        }
-
-        //exemple MASTERCARD:
-        Client c2 = new Client("Julien Herbaux");
-        c2.setContact("julien@gmail.com");
-        c2.setPaiement("2483571672956135");
-        Reservation r2 = new Reservation(c2, v1);
-
-        //exemple AMEX:
-        Client c3 = new Client("Axel Exposito");
-        c3.setContact("axel@gmail.com");
-        c3.setPaiement("377732303291515");
-        Reservation r3 = new Reservation(c3, v1);
 
     }
 }
